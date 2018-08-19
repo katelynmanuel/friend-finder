a073f926886e7922b8959898bdfa51d6a1185246
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-08-19 17:54:01 -0600

    Adding functionality to display image in modal and changes to friends data.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 let friends = [
     {
-        "name": "Dwight Schrute",
-        "photo": "https://theofficeanalytics.files.wordpress.com/2017/11/dwight.jpeg?w=200",
-        "scores": [1, 4, 5, 5, 4, 1, 3, 1, 3, 2]
+        name: "Dwight Schrute",
+        image: "https://theofficeanalytics.files.wordpress.com/2017/11/dwight.jpeg?w=200",
+        scores: [1, 4, 5, 5, 4, 1, 3, 1, 3, 2]
     },
     {
-        "name": "Jim Halpert",
-        "photo": "https://topicimages.mrowl.com/medium/netflixislife/theoffice/myfavoritechar/jimhalpert_1.jpg",
-        "scores": [1, 2, 3, 1, 2, 5, 5, 3, 5, 4]
+        name: "Jim Halpert",
+        image: "https://topicimages.mrowl.com/medium/netflixislife/theoffice/myfavoritechar/jimhalpert_1.jpg",
+        scores: [1, 2, 3, 1, 2, 5, 5, 3, 5, 4]
     },
     {
-        "name": "Pam Beesly",
-        "photo": "https://theofficeanalytics.files.wordpress.com/2017/09/pam-halpert-1.jpg?w=200",
-        "scores": [2, 2, 4, 1, 2, 5, 5, 1, 5, 3]
+        name: "Pam Beesly",
+        image: "https://theofficeanalytics.files.wordpress.com/2017/09/pam-halpert-1.jpg?w=200",
+        scores: [2, 2, 4, 1, 2, 5, 5, 1, 5, 3]
     },
     {
-        "name": "Kelly Kapoor",
-        "photo": "https://theofficeanalytics.files.wordpress.com/2017/11/kelly_kapoor.jpg",
-        "scores": [5, 4, 1, 4, 4, 3, 5, 3, 5, 5]
+        name: "Kelly Kapoor",
+        image: "https://theofficeanalytics.files.wordpress.com/2017/11/kelly_kapoor.jpg",
+        scores: [5, 4, 1, 4, 4, 3, 5, 3, 5, 5]
     }
 ];
 
